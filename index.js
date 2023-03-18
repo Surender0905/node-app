b5433557b6505f640fb397eb42b252c1954c793b
Surender Pratap Singh <rk31151@gmail.com>
2023-03-18 19:38:29 +0530

    end of web server

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const lib = require('./lib');
+const http = require('http');
 
-const express = require('express');
+const fs = require('fs');
 
-const app = express();
-const port = 8080;
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 
-console.log('hello');
+// const data = { name: 'roshan' }; // server doen't knw it is jason data.. so every thing should be string
+// const data_string = JSON.stringify(data);
 
-app.listen(port, () => console.log('server is running' + ' ' + port));
+const products = data.products;
 
-// const fs = require('fs');
+const server = http.createServer((req, res) => {
+  console.log(req.url);
 
-// const t1 = performance.now();
-// fs.readFile('./demo.txt', 'utf-8', (error, txt) => {
-//   console.log(txt);
-// });
+  // res.setHeader('token', 'dummy');
 
-// const t2 = performance.now();
+  if (req.url.startsWith('/product')) {
+    const id = req.url.split('/')[2];
+    const prd = products.find((p) => p.id === +id);
+    const page = index
+      .replace('**title**', prd.title)
+      .replace('**url**', prd.thumbnail)
+      .replace('**price**', prd.price);
 
-// console.log(t2 - t1, t1, t2);
+    res.end(page);
+    return;
+  }
+  switch (req.url) {
+    case '/':
+      res.setHeader('Content-Type', 'text/html');
 
-// console.log('hello world');
-// console.log(lib.sum(2, 4));
+      res.end(index);
+      break;
+    case '/api':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(data));
+      break;
+
+    default:
+      res.writeHead(404, 'Not found');
+      res.end();
+  }
+  console.log('server started');
+
+  //   res.setHeader('Content-Type', 'application/json');
+});
+
+server.listen(8080); //// running on this port by binding with server
